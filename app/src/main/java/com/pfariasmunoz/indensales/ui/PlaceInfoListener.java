1fa22590a41036fd8febee07fb23f300d5f440b3
Pablo Munoz <pfariasmunoz@gmail.com>
2017-06-22 10:39:02 -0400

    Add interface for implementing popup menu

---
+++ app/src/main/java/com/pfariasmunoz/indensales/ui/PlaceInfoListener.java
@@ -0,0 +1,11 @@
+package com.pfariasmunoz.indensales.ui;
+
+import android.view.View;
+
+/**
+ * Created by Pablo Farias on 22-06-17.
+ */
+
+public interface PlaceInfoListener {
+    void onPopupMenuClick(View view, int pos);
+}
